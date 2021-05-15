8488d4c288784b2a2a23ec640f29935dcff4a76c
Rabi14378 <rabibro14378@gmail.com>
2021-05-15 13:33:47 +0545

    First commit

--- hello.cpp
+++ hello.cpp
@@ -6,9 +6,3 @@ int main(){
   std::cout<<"Hellow world "<<a<<std::endl;
   return 0;
 }
-/*git init
-git add .
-git commit -m "first commit"
-git remote add origin https://...
-git push -u origin main
-*/
